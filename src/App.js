9e1d9dbb382fff4f1e57269d6803a7e8a31a13a4
OlaOluwalekan <olalekanbello534@gmail.com>
2022-12-17 07:25:36 +0100

    completed use effect tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Final from "./1-useState/final/4-useState-object";
-import Setup from "./1-useState/setup/5-useState-counter";
+// import Final from "./2-useEffect/final/3-useEffect-fetch-data";
+import Setup from "./2-useEffect/setup/3-useEffect-fetch-data";
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      <Final />
+      {/* <Final /> */}
     </div>
   );
 }
--- src/projects/01-birthday-reminder/Card.js
+++ src/projects/01-birthday-reminder/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Card = (props) => {
-  const { id, image, name, age } = props;
+  const { image, name, age } = props;
   return (
     <div className="card">
-      <img src={image} className="image" />
+      <img src={image} className="image" alt={name} />
       <article>
         <p className="name">{name}</p>
         <p className="age">{age} years</p>
