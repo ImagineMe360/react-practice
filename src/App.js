e789e2b0273a0926acad72417987859c49622ee0
OlaOluwalekan <olalekanbello534@gmail.com>
2022-12-18 23:54:41 +0100

    completed react forms tutorial

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import React, { useState } from "react";
-// import Final from "./3-conditional-rendering/final/3-show-hide";
-// import Setup from "./3-conditional-rendering/setup/3-show-hide";
-import TourList from "./projects/02-tours/TourList";
-import NavIcon from "./projects/02-tours/NavIcon";
-import NavBar from "./projects/02-tours/NavBar";
-import Reviews from "./projects/02-tours/Reviews";
+// import Final from "./4-forms/final/2-multiple-inputs"
+import Setup from "./4-forms/setup/2-multiple-inputs"
 
 function App() {
-  const [navIsOpen, setNavIsOpen] = useState(false);
-  const [tourIsOpen, setTourIsOpen] = useState(false);
-  const [reviewIsOpen, setReviewIsOpen] = useState(false);
-  const openNav = () => {
-    setNavIsOpen(!navIsOpen);
-  };
-
-  const openTours = () => {
-    setTourIsOpen(true);
-    setNavIsOpen(false);
-    setReviewIsOpen(false);
-  };
-
-  const openReviews = () => {
-    setReviewIsOpen(true);
-    setNavIsOpen(false);
-    setTourIsOpen(false);
-  };
   return (
-    <div className="container">
-      <div className="nav-icon-container">
-        <NavIcon openNav={openNav} />
-      </div>
-      {/* <div className="nav"></div> */}
-      {navIsOpen && <NavBar openTours={openTours} openReviews={openReviews} />}
-
-      <section className="head">
-        <h1>Our Tours</h1>
-        <div className="underline"></div>
-      </section>
-      {tourIsOpen && <TourList />}
-      {reviewIsOpen && (
-        <div className="review-scroll-track">
-          <Reviews />
-        </div>
-      )}
+    <div className='container'>
+      <Setup />
+      {/* <Final /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
